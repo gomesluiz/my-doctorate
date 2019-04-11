3dde718f7156bf4ed1f7c41e0d0d1728d31138b4
Luiz Alberto <gomes.luiz@gmail.com>
2019-04-11 09:06:07 -0300

    data updated

--- long-lived-bug-prediction/scripts/predict_long_lived_bug.R
+++ long-lived-bug-prediction/scripts/predict_long_lived_bug.R
@@ -4,7 +4,6 @@
 #'
 #' @author Luiz Alberto (gomes.luiz@gmail.com)
 #'
-
 rm(list = ls(all.names = TRUE))
 options(readr.num_columns = 0)
 
@@ -15,6 +14,7 @@ DATADIR <- file.path(BASEDIR, "notebooks", "datasets")
 
 if (!require('caret')) install.packages("caret", dependencies = c("Depends", "Suggests"))
 if (!require('doParallel')) install.packages("doParallel", dependencies = c("Depends", "Suggests"))
+if (!require("klaR")) install.packages("klaR", dependencies = TRUE) # naive bayes package.
 if (!require('e1071')) install.packages("e1071", dependencies = c("Depends", "Suggests"))
 if (!require("futile.logger")) install.packages("futile.logger", dependencies = TRUE)
 if (!require("qdap")) install.packages("qdap", dependencies = TRUE)
@@ -51,168 +51,174 @@ r_cluster <- makePSOCKcluster(4)
 registerDoParallel(r_cluster)
 
 timestamp       <- format(Sys.time(), "%Y%m%d%H%M%S")
-oss.projects    <- c("eclipse", "freedesktop", "gnome", "mozilla", "netbeans", "winehq")
-dataset.name    <- "winehq"
-metrics.mask    <- sprintf("%s_result_metrics.csv", dataset.name)
-models.mask     <- sprintf("%s_final_model.rds", dataset.name)
+projects        <- c("eclipse", "freedesktop", "gnome", "mozilla", "netbeans", "winehq")
 class_label     <- "long_lived"
 fixed.threshold <- 64
 
-metrics.file  <- get_last_evaluation_file(DATADIR, metrics.mask)
-
 feature    <- c("short_long_description")
-resampling <- unlist(resampling_methods)
+#resampling <- unlist(resampling_methods)
+resampling <- c("LGOCV")
 classifier <- c("knn", "svmRadial", "nb", "rf")
 n_term     <- c(100, 200, 300, 400, 500)
 #balancing  <- c(UNBALANCED, MANUALMETHOD, DOWNSAMPLE, SMOTEMETHOD)
 balancing  <- c(UNBALANCED)
 threshold  <- seq(4, fixed.threshold, by = 4)
-parameters <- crossing(oss.projects, n_term, classifier, resampling, balancing, feature, threshold)
+parameters <- crossing(n_term, classifier, resampling, balancing, feature, threshold)
 
 flog.threshold(TRACE)
 flog.trace("Script started...")
 flog.trace("Ouput path: %s", DATADIR)
 
-if (is.na(metrics.file)) {
-  metrics.file = format_file_name(DATADIR, timestamp, metrics.mask)
-  flog.trace("Current Evaluation file: %s", metrics.file)
-  start.parameter  <- 1
-} else {
-  flog.trace("Last evaluation file generated: %s", metrics.file)
-  all.lines <- read_csv(metrics.file)
-  start.parameter <- get_next_n_parameter(all.lines, parameters)
-  if (start.parameter == 1) {
-    metrics.file  <- format_file_name(DATADIR, timestamp, metrics.mask)
-    flog.trace("New evaluation file generated: %s", metrics.file)
-  }
-}
-flog.trace("Starting with parameter: %d", start.parameter)
-
-greatest_balanced_acc <- 0
-last.feature          <- "@"
-last.oss.project      <- "@"
-last.n_term           <- 0
-model.file   <- format_file_name(DATADIR , timestamp , models.mask)
-
-for (i in start.parameter:nrow(parameters)) {
-  parameter = parameters[i, ]
+for (project.name in projects){
+  flog.trace(">>> PROJECT NAME : %s", project.name)
   
-  if (parameter$oss.project != last.oss.project)
-    reports.file <- file.path(DATADIR, sprintf("20190309_%s_bug_report_data.csv", parameter$oss.project))
-    flog.trace("Bug reports file : %s", reports.file)
-    
-    # cleaning data
-    reports <- read_csv(reports.file, na  = c("", "NA"))
-    reports <- reports[, c('bug_id', 'short_description', 'long_description', 'days_to_resolve')]
-    reports <- reports[complete.cases(reports), ]
-    reports <- reports %>% filter((days_to_resolve) >= 0 & (days_to_resolve <= 730))
-    
-    flog.trace("Clean text features")
-    reports$short_description <- clean_text(reports$short_description)
-    reports$long_description  <- clean_text(reports$long_description)
-    reports$short_long_description <- paste(reports$short_description, reports$long_description, sep=" ")
-    
-    last.oss.project = parameter$oss.project
+  metrics.mask  <- sprintf("%s__result_metrics.csv", project.name)
+  metrics.file  <- get_last_evaluation_file(DATADIR, metrics.mask)
+  
+  if (is.na(metrics.file)) {
+    metrics.file = format_file_name(DATADIR, timestamp, metrics.mask)
+    flog.trace("Current Evaluation file: %s", metrics.file)
+    start.parameter  <- 1
+  } else {
+    flog.trace("Last evaluation file generated: %s", metrics.file)
+    all.lines <- read_csv(metrics.file)
+    start.parameter <- get_next_n_parameter(all.lines, parameters)
+    if (start.parameter == 1) {
+      metrics.file  <- format_file_name(DATADIR, timestamp, metrics.mask)
+      flog.trace("New evaluation file generated: %s", metrics.file)
+    }
   }
-  if (parameter$n_term != last.n_term) {
-    flog.trace("Text mining: extracting %d terms", parameter$n_term)
-    source <- reports[, c('bug_id', parameters$feature)]
-    corpus <- clean_corpus(source)
+  
+  flog.trace("Starting with parameter: %d", start.parameter)
 
-    dtm  <- tidy(make_dtm(corpus, parameter$n_term))
-    names(dtm) <- c("bug_id", "term", "count")
-    term.matrix <- dtm %>% spread(key = term, value = count, fill = 0)
+  #greatest_balanced_acc <- 0
+  #last.feature          <- "@"
+  #last.oss.project      <- "@"
+  
+  #model.file   <- format_file_name(DATADIR , timestamp , models.mask)
 
-    reports.terms <- merge(
-      x = reports[, c('bug_id', 'days_to_resolve')],
-      y = term.matrix,
-      by.x = 'bug_id',
-      by.y = 'bug_id'
-    )
-    last.n_term  = parameter$n_term 
-  }
+  reports.file <- file.path(DATADIR, sprintf("20190309_%s_bug_report_data.csv", project.name))
+  flog.trace("Bug reports file : %s", reports.file)
+  
+  # cleaning data
+  reports <- read_csv(reports.file, na  = c("", "NA"))
+  reports <- reports[, c('bug_id', 'short_description', 'long_description', 'days_to_resolve')]
+  reports <- reports[complete.cases(reports), ]
+  reports <- reports %>% filter((days_to_resolve) >= 0 & (days_to_resolve <= 730))
+  
+  flog.trace("Clean text features")
+  reports$short_description <- clean_text(reports$short_description)
+  reports$long_description  <- clean_text(reports$long_description)
+  reports$short_long_description <- paste(reports$short_description, reports$long_description, sep=" ")
+  
+  #last.oss.project = parameter$oss.project
+  
+  last.n_term           <- 0
+  
+  for (i in start.parameter:nrow(parameters)) {
+    parameter = parameters[i, ]
+  
+    set.seed(1234)
+    
+    if (parameter$n_term != last.n_term) {
+      flog.trace("Text mining: extracting %d terms", parameter$n_term)
+      source <- reports[, c('bug_id', parameters$feature)]
+      corpus <- clean_corpus(source)
 
-  reports.terms$long_lived <- as.factor(ifelse(reports.terms$days_to_resolve <= fixed.threshold, 0, 1))
+      dtm  <- tidy(make_dtm(corpus, parameter$n_term))
+      names(dtm) <- c("bug_id", "term", "count")
+      term.matrix <- dtm %>% spread(key = term, value = count, fill = 0)
 
-  short_liveds <- subset(reports.terms , days_to_resolve <= parameter$threshold)
-  long_liveds  <- subset(reports.terms , long_lived == 1)
-  all_reports  <- rbind(short_liveds, long_liveds)
+      reports.terms <- merge(
+        x = reports[, c('bug_id', 'days_to_resolve')],
+        y = term.matrix,
+        by.x = 'bug_id',
+        by.y = 'bug_id'
+      )
+      last.n_term  = parameter$n_term 
+    }
 
-  set.seed(1234)
-  flog.trace("Predicting: feature[%s], threshold[%s], classifier[%s], resampling[%s], balancing[%s]",
+    reports.terms$long_lived <- as.factor(ifelse(reports.terms$days_to_resolve <= fixed.threshold, 0, 1))
+
+    short_liveds <- subset(reports.terms , days_to_resolve <= parameter$threshold)
+    long_liveds  <- subset(reports.terms , long_lived == 1)
+    all_reports  <- rbind(short_liveds, long_liveds)
+
+    flog.trace("Predicting: feature[%s], threshold[%s], classifier[%s], resampling[%s], balancing[%s]",
              parameter$feature, parameter$threshold, parameter$classifier, parameter$resampling,
              parameter$balancing)
 
-  flog.trace("Balancing dataset")
-  dataset = balance_dataset(all_reports, class_label, parameter$balancing)
-  predictors <- !(names(dataset) %in% c(class_label))
+    flog.trace("Balancing dataset")
+    dataset = balance_dataset(all_reports, class_label, parameter$balancing)
+    predictors <- !(names(dataset) %in% c(class_label))
 
-  flog.trace("Partitioning dataset")
-  in_train <- createDataPartition(dataset[, class_label], p = 0.75, list = FALSE)
+    flog.trace("Partitioning dataset")
+    in_train <- createDataPartition(dataset[, class_label], p = 0.75, list = FALSE)
 
-  X_train <- dataset[in_train, predictors]
-  y_train <- dataset[in_train, class_label]
+    X_train <- dataset[in_train, predictors]
+    y_train <- dataset[in_train, class_label]
 
-  X_test  <- dataset[-in_train, predictors]
-  y_test  <- dataset[-in_train, class_label]
+    X_test  <- dataset[-in_train, predictors]
+    y_test  <- dataset[-in_train, class_label]
 
-  flog.trace("Training model: ")
-  fit_model <- train(  x = X_train
-                     , y = y_train
-                     , method = parameter$classifier
-                     , trControl =  get_resampling_method(parameter$resampling))
+    flog.trace("Training model: ")
+    fit_model <- train(  x = X_train
+                       , y = y_train
+                       , method = parameter$classifier
+                       , trControl =  get_resampling_method(parameter$resampling))
 
-  flog.trace("Testing model: ")
-  y_hat <- predict(object = fit_model, X_test)
+    flog.trace("Testing model: ")
+    y_hat <- predict(object = fit_model, X_test)
 
-  cm <- confusionMatrix(data = y_hat, reference = y_test, mode = "prec_recall")
-  tp <- cm$table[1, 1]
-  fp <- cm$table[1, 2]
-  tn <- cm$table[2, 1]
-  fn <- cm$table[2, 2]
+    cm <- confusionMatrix(data = y_hat, reference = y_test, mode = "prec_recall")
+    tp <- cm$table[1, 1]
+    fp <- cm$table[1, 2]
+    tn <- cm$table[2, 1]
+    fn <- cm$table[2, 2]
 
-  precision <- tp / (tp + fp)
-  recall <- tp / (tp + fn)
-  fmeasure <- (2 * recall * precision) / (recall + precision)
-  acc_class_0 <- tp / (tp + fp)
-  acc_class_1 <- tn / (tn + fn)
-  balanced_acc <- (acc_class_0 + acc_class_1) / 2
+    precision <- tp / (tp + fp)
+    recall <- tp / (tp + fn)
+    fmeasure <- (2 * recall * precision) / (recall + precision)
+    acc_class_0 <- tp / (tp + fp)
+    acc_class_1 <- tn / (tn + fn)
+    balanced_acc <- (acc_class_0 + acc_class_1) / 2
   
-  if (balanced_acc > greatest_balanced_acc){
-    saveRDS(fit_model, model.file)
-    greatest_balanced_acc <- balanced_acc
+    # if (balanced_acc > greatest_balanced_acc){
+    #   saveRDS(fit_model, model.file)
+    #   greatest_balanced_acc <- balanced_acc
+    # }
+
+    flog.trace("Evaluating: Bcc [%f], Acc0 [%f], Acc1 [%f]", balanced_acc, acc_class_0, acc_class_1)
+    one.evaluation <-
+      data.frame(
+        dataset = project.name,
+        classifier = parameter$classifier,
+        resampling = parameter$resampling,
+        balancing  = parameter$balancing,
+        threshold  = parameter$threshold,
+        fixed_threshold   = fixed.threshold,
+        train_size = nrow(X_train),
+        train_size_class_0 = length(subset(y_train, y_train == 0)),
+        train_size_class_1 = length(subset(y_train, y_train == 1)),
+        test_size = nrow(X_test),
+        test_size_class_0 = length(subset(y_test, y_test == 0)),
+        test_size_class_1 = length(subset(y_test, y_test == 1)),
+        feature = parameter$feature,
+        n_term = parameter$n_term,
+        tp = tp,
+        fp = fp,
+        tn = tn,
+        fn = fn,
+        acc_class_0 = acc_class_0,
+        acc_class_1 = acc_class_1,
+        balanced_acc = balanced_acc,
+        precision = precision,
+        recall = recall,
+        fmeasure = fmeasure
+      )
+
+      flog.trace("Recording evaluation results on CSV file.")
+      insert_one_evaluation_data(metrics.file, one.evaluation)
   }
-
-  flog.trace("Evaluating: Bcc [%f], Acc0 [%f], Acc1 [%f]", balanced_acc, acc_class_0, acc_class_1)
-  one.evaluation <-
-    data.frame(
-      dataset = parameter$oss.project,
-      classifier = parameter$classifier,
-      resampling = parameter$resampling,
-      balancing  = parameter$balancing,
-      threshold  = parameter$threshold,
-      fixed_threshold   = fixed.threshold,
-      train_size = nrow(X_train),
-      train_size_class_0 = length(subset(y_train, y_train == 0)),
-      train_size_class_1 = length(subset(y_train, y_train == 1)),
-      test_size = nrow(X_test),
-      test_size_class_0 = length(subset(y_test, y_test == 0)),
-      test_size_class_1 = length(subset(y_test, y_test == 1)),
-      feature = parameter$feature,
-      n_term = parameter$n_term,
-      tp = tp,
-      fp = fp,
-      tn = tn,
-      fn = fn,
-      acc_class_0 = acc_class_0,
-      acc_class_1 = acc_class_1,
-      balanced_acc = balanced_acc,
-      precision = precision,
-      recall = recall,
-      fmeasure = fmeasure
-    )
-
-  flog.trace("Recording evaluation results on CSV file.")
-  insert_one_evaluation_data(metrics.file, one.evaluation)
 }
 flog.trace("End of predicting long lived bug.")
