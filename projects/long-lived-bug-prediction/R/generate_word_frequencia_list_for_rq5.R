8d4402d1bb23cfc8345969f8508d82893749e02e
Luiz Alberto <gomes.luiz@gmail.com>
2019-12-12 09:30:46 -0300

    contents updated

--- projects/long-lived-bug-prediction/R/generate_word_frequencia_list_for_rq5.R
+++ projects/long-lived-bug-prediction/R/generate_word_frequencia_list_for_rq5.R
@@ -13,7 +13,7 @@
 # clean R Studio session.
 rm(list = ls(all.names = TRUE))
 options(readr.num_columns = 0)
-timestamp  <- format(Sys.time(), "%Y%m%d%H%M%S")
+timestamp  <- format(Sys.time(), "%Y%m%d")
 
 # setup project folders.
 IN_DEBUG_MODE  <- FALSE
@@ -46,6 +46,7 @@ library(tm)
 library(wordcloud)
 library(RColorBrewer)
 library(ggplot2)
+library(reshape2)
 
 source(file.path(LIBDIR, "clean_corpus.R"))
 source(file.path(LIBDIR, "clean_text.R"))
@@ -63,7 +64,7 @@ registerDoParallel(r_cluster)
 #'
 #' @return The Term Document Matrix
 #'
-make_tdm <- function(.docs, .n=100) {
+make_summarized_tdm <- function(.docs, .n=100) {
   corpus  <- clean_corpus(.docs)
   tdm     <- TermDocumentMatrix(corpus, control=list(weighting=weightTfIdf))
   tdm     <- as.matrix(tdm)
@@ -72,13 +73,32 @@ make_tdm <- function(.docs, .n=100) {
   return (head(tdm, .n))
 }
 
+make_detailed_tdm <- function(.docs, .n=100) {
+  corpus  <- clean_corpus(.docs)
+  tdm     <- TermDocumentMatrix(corpus, control=list(weighting=weightTfIdf))
+  flog.trace(">> tdm building done")
+  
+  tdm.matrix <- as.matrix(tdm)
+  tdm.sorted <- sort(rowSums(tdm.matrix), decreasing=TRUE)
+  flog.trace(">> tdm ordering done")
+  
+  tdm.words <- head(data.frame(word=names(tdm.sorted)), .n)
+  flog.trace(">> tdm wording done")
+  
+  tdm.melted   <- melt(tdm.matrix)
+  tdm.melted   <- subset(tdm.melted, Terms %in% tdm.words$word)
+  flog.trace(">> tdm melting done")
+  
+  return (tdm.melted)
+}
+
 # main function
-for (project.name in c('winehq'))
+for (project.name in c('eclipse', 'gcc'))
 {
   flog.trace("reading data files of %s project", project.name)
   reports.path <- file.path(DATADIR, sprintf("20190917_%s_bug_report_data.csv", project.name))
   results.path <- file.path(DATADIR
-                            , sprintf("20191203230737_rq4e4_%s_test_results_balanced_acc.csv"
+                            , sprintf("20190926143854_rq4e4_%s_tests_balanced_acc.csv"
                                       , project.name
                                       )
                             )
@@ -105,19 +125,32 @@ for (project.name in c('winehq'))
   reports.merged$long_description  <- clean_text(reports.merged$long_description)
 
   # filtering out long-lived correct predicted
-  flog.trace("making document term matrix for correct predicted bugs")
+  flog.trace("making document term matrix for true positives predicted bugs")
   reports.merged         <- subset(reports.merged, long_lived=='Y')
-  predicted.corrected    <- make_tdm(subset(reports.merged, y_hat=='Y', select=c(bug_id, long_description)), 100)
-  predicted.corrected$status <- 'Corrected'
+  predicted.corrected    <- make_detailed_tdm(subset(reports.merged, y_hat=='Y'
+                                            , select=c(bug_id, long_description)), 100)
+  predicted.corrected$status_prediction <- 'True Positive'
+  #ggplot(predicted.corrected, aes(as.factor(Docs), Terms, fill=log(value))) + 
+  #  geom_tile()  + 
+  #  xlab("Docs") + 
+  #  scale_fill_continuous(low="#FEE6CE", high="#E6550D")+
+  #  theme(axis.text.x=element_blank(), axis.text.y=element_blank())
+  
+  flog.trace("making document term matrix for false negatives predicted bugs")
+  predicted.uncorrected  <- make_detailed_tdm(subset(reports.merged, y_hat=='N'
+                                            , select=c(bug_id, long_description)), 100)
+  predicted.uncorrected$status_prediction <- 'False Negative'
+  #ggplot(predicted.uncorrected, aes(as.factor(Docs), Terms, fill=log(value))) + 
+  #  geom_tile()  + 
+  #  xlab("Docs") + 
+  #  scale_fill_continuous(low="#FEE6CE", high="#E6550D")+
+  #theme(axis.text.x=element_blank(), axis.text.y=element_blank())
+  
 
-  flog.trace("making document term matrix for uncorrect predicted bugs")
-  predicted.uncorrected  <- make_tdm(subset(reports.merged, y_hat=='N', select=c(bug_id, long_description)), 100)
-  predicted.uncorrected$status <- 'Uncorrected'
-
-  flog.trace("recording word frequency list")
+  flog.trace("recording TDM")
   frequency.list <- rbind(predicted.corrected, predicted.uncorrected)
   frequency.list$project <- project.name
-  frequency.path = file.path(DATADIR, sprintf("%s_%s_frequency_word_list.csv", timestamp, project.name))
+  frequency.path = file.path(DATADIR, sprintf("%s_%s_tdm_detailed.csv", timestamp, project.name))
   write_csv(frequency.list , frequency.path)
 }
 flog.trace("processing finished")
