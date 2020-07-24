91490ec9235290558ce675c64235fd78f7f8cbfe
Luiz Alberto <gomes.luiz@gmail.com>
2020-07-24 05:52:13 +0200

    contents updated

--- projects/long-lived-bug-prediction/machine-learning/run_1.sh
+++ projects/long-lived-bug-prediction/machine-learning/run_1.sh
@@ -3,4 +3,4 @@
 IMAGE=long-lived-bug-prediction-ml-v1
 SHAREDIR=/home/lgomes/Workspace/doctorate/projects/long-lived-bug-prediction/machine-learning/source/datasets
 WORKDIR=/home/lgomes/Workspace/doctorate/projects/long-lived-bug-prediction/machine-learning/R
-docker run  -it --ipc=host --userns=host -v ${SHAREDIR}/:${SHAREDIR}/ --workdir ${WORKDIR}/ ${IMAGE}:latest Rscript ${WORKDIR}/predict_long_lived_bug_e1_best_tune_rev1.R
+docker run -d -it --ipc=host --userns=host -v ${SHAREDIR}/:${SHAREDIR}/ --workdir ${WORKDIR}/ ${IMAGE}:latest Rscript ${WORKDIR}/predict_long_lived_bug_e1_best_tune_rev1.R
